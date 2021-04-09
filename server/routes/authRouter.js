8d3ce15cf2e62a1522a39018855e180987269881
Alevlapon <ponibrashin@yandex.ru>
2021-04-09 15:49:11 +0300

    Регистрация back done(fix)

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -3,19 +3,17 @@ const router = new Router()
 import User from "../models/users.js"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
-import secret from "../config.js"
 
 
 const generateToken = (id) => {
     const payload = {id}
-    return jwt.sign(payload, secret.secret , {expiresIn: "1h"} )
+    return jwt.sign(payload, process.env.SECRET , {expiresIn: "1h"} )
 }
 
 router.route("/signup")
     .post(async (req, res) => {
         try {
             const { name, email, password } = req.body
-            console.log(name,email);
             const account = await User.findOne({ email })
             if (account) {
                 return res.status(400).json({ message: "Email already exist" })
@@ -33,19 +31,15 @@ router.route("/login")
     .post(async (req, res) => {
         try {
             const { email, password } = req.body
-            console.log(email, password);
             const account = await User.findOne({ email: email })
-            console.log(account);
             if (!account) {
                 return res.status(400).json({ message: "Wrong email" })
             }
             const truePassword = bcrypt.compareSync(password, account.password)
-            console.log(truePassword)
             if (!truePassword) {
                 return res.status(400).json({ message: "Wrong password" })
             }
             const token = generateToken(account._id)
-            console.log(token);
             return res.status(200).json({token})
         } catch (e) {
             res.status(400).json({ message: "login error" })
