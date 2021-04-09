f3ff028c5045b3bc86ef0dc5558c877830ec1746
NilsOn <johnynar@gmail.com>
2021-04-09 15:34:05 +0300

    correct EditForm

--- client/src/components/FormEdit/FormEdit.js
+++ client/src/components/FormEdit/FormEdit.js
@@ -17,7 +17,7 @@ function FormEdit({ }) {
 
           <div id='sexDiv' className="field">
             <div className="col-4 col-12-small">
-              <input type="radio" id="demo-priority-low" name="sex" checked />
+              <input type="radio" id="demo-priority-low" name="sex" dafaultChecked />
               <label htmlFor="demo-priority-low">Woman</label>
             </div>
             <div className="col-4 col-12-small">
