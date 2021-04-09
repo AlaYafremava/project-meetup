178ad43eb09a8b7bb2e673d1a333c5c39b08e2b1
Alla <yefremova.alla@gmail.com>
2021-04-09 15:50:03 +0300

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header/Header'
+import Profile from './components/Profile/Profile'
 import Auth from './components/Auth/Auth'
-import Main from './components/Main/Main'
+import Main from './components/Profile/Profile'
 import store from './redux/store'
 import { Provider } from 'react-redux'
 import Travels from './components/Travels/Travels'
@@ -12,20 +12,22 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Header />
         <Switch>
           <Route exact path="/">
             <Auth />
           </Route>
-          <div id="main">
-            <Route path="/dashboard">
-              <Main />
-            </Route>
 
-            <Route path="/travels">
-              <Travels />
-            </Route>
-          </div>
+          <Route path="/dashboard">
+            <Main />
+          </Route>
+
+          <Route path="/travels">
+            <Travels />
+          </Route>
+
+          <Route path="/profile">
+            <Profile />
+          </Route>
         </Switch>
       </Router>
     </Provider>
--- client/src/components/Travels/Travels.js
+++ client/src/components/Travels/Travels.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import Header from '../Header/Header'
 
 function Travels(props) {
   return (
-    <section className="post">
-      <div className="row">
-        <div class="col-3 col-12-small">боковая панель</div>
-        <div class="col-9 col-12-small">шлавная панель</div>
+    <>
+      <Header />
+      <div id="main">
+        <section className="post">
+          <div className="row">
+            <div class="col-3 col-12-small">боковая панель</div>
+            <div class="col-9 col-12-small">шлавная панель</div>
+          </div>
+        </section>
       </div>
-    </section>
+    </>
   )
 }
 
