32c9a44cc347b1fcc70e900250a169794c855e6b
Kristina <kristina.car@mail.ru>
2021-04-10 18:47:03 +0300

    not working

--- client/src/components/UserInterests/UserInterests.js
+++ client/src/components/UserInterests/UserInterests.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './UserInterests.css'
 
 function UserInterests(props) {
   return (
     <div>
-      <h3>Interests</h3>
+      <h3> Interests </h3>
+      <ul className='actions'>
+        <li>
+          <input className='tags' type='submit' value='Vegan' style={{ borderRadius: '10px' }}/>
+        </li>
+      </ul>
     </div>
   );
 }
