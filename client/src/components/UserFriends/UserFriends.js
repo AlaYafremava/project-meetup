94294ce1693cec35d885e8ef9aebe4d63124a959
Alla <yefremova.alla@gmail.com>
2021-04-14 10:39:03 +0300

    Nothing special

--- client/src/components/UserFriends/UserFriends.js
+++ client/src/components/UserFriends/UserFriends.js
@@ -1,77 +1,107 @@
 import React from 'react'
-import style from './UserFriends.module.css'
+import './UserFriends.css'
+
+const user = {}
 
 function UserFriends(props) {
   return (
     <section>
-      {/* <div className="card-friends">
-        <img src="avatar.jpeg" alt="avatar" className="avatar" />
-        <p className="userName">
-          <i>Alla Yefremova</i>
-        </p>
-        <p className="title-friends">
-          <i>
-            Saint-Petersburg, <br />
-            Russian Federation
-          </i>
-        </p>
-        <hr />
+      <div className="card-friend">
+        <div>
+          <img src="avatar.jpeg" alt="avatar" className="avatar-friend" />
+        </div>
+        <div className="title-friend">
+          <p>
+            <i>Alex Ponibrashin</i>
+          </p>
+        </div>
+        <div className="title-friend">
+          <p>
+            <i>RUSSIAN FEDERATION</i>
+          </p>
+        </div>
         <ul className="icons alt">
           <li>
-            <a href="#" className="icon brands alt fa-telegram">
+            <a href={user.telegram} className="icon brands alt fa-telegram">
               <span className="label">Telegram</span>
             </a>
           </li>
           <li>
-            <a href="#" className="icon brands alt fa-facebook-f">
+            <a href={user.facebook} className="icon brands alt fa-facebook-f">
               <span className="label">Facebook</span>
             </a>
           </li>
           <li>
-            <a href="#" className="icon brands alt fa-instagram">
+            <a href={user.instagram} className="icon brands alt fa-instagram">
               <span className="label">Instagram</span>
             </a>
           </li>
         </ul>
-      </div> */}
-
-      <div className="team-block col-lg-4 col-md-6 col-sm-12">
-        <div className="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
-          <ul className="social-icons">
-            <li>
-              <a href="#">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-skype"></i>
-              </a>
-            </li>
-
-            <li>
-              <a href="#">
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-            </li>
-          </ul>
-          <div className="image">
-            <a href="#">
-              <img src="https://i.ibb.co/JC4skS0/team-animate.jpg" alt="" />
-            </a>
-          </div>
-          <div className="lower-content">
-            <h3>
-              <a href="#">Lorem Ipsum </a>
-            </h3>
-            <div className="designation">Simply dummy text </div>
-          </div>
+      </div>
+      <div className="card-friend">
+        <div>
+          <img src="avatar.jpeg" alt="avatar" className="avatar-friend" />
         </div>
+        <div className="title-friend">
+          <p>
+            <i>Alla Yefremova</i>
+          </p>
+        </div>
+        <div className="title-friend">
+          <p>
+            <i>BELARUS</i>
+          </p>
+        </div>
+        <ul className="icons alt">
+          <li>
+            <a href={user.telegram} className="icon brands alt fa-telegram">
+              <span className="label">Telegram</span>
+            </a>
+          </li>
+          <li>
+            <a href={user.facebook} className="icon brands alt fa-facebook-f">
+              <span className="label">Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a href={user.instagram} className="icon brands alt fa-instagram">
+              <span className="label">Instagram</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="card-friend">
+        <div>
+          <img src="avatar.jpeg" alt="avatar" className="avatar-friend" />
+        </div>
+        <div className="title-friend">
+          <p>
+            <i>Ivan Vorotov</i>
+          </p>
+        </div>
+        <div className="title-friend">
+          <p>
+            <i>RUSSIAN FEDERATION</i>
+          </p>
+        </div>
+        <ul className="icons alt">
+          <li>
+            <a href={user.telegram} className="icon brands alt fa-telegram">
+              <span className="label">Telegram</span>
+            </a>
+          </li>
+          <li>
+            <a href={user.facebook} className="icon brands alt fa-facebook-f">
+              <span className="label">Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a href={user.instagram} className="icon brands alt fa-instagram">
+              <span className="label">Instagram</span>
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   )
